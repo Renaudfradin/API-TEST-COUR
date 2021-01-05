0b44dad3ae366cae1a32ae0ba9774a34a9487c57
Renaud <renaudfradin@gmail.com>
2021-01-05 21:28:58 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const Thing = require("./models/Thing.js");
+
+
+const app = express();
+
+mongoose.connect('mongodb+srv://RenaudFRADIN:Tigrette1792365020172020@cluster0.wtcju.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+ delete req.body._id;
+ const thing = new Thing({
+    ...req.body
+ });
+ thing.save()
+    .then(()=>res.status(201).json({message:"objt creee !!!!!!!"}))
+    .catch(error=>res.status(400).json({ error }))
+});
+
+
+
+app.use('/api/stuff', (req, res, next) => {
+    Thing.find()
+        .then( things =>res.status(200).json({things}))
+        .catch(error=>res.status(400).json({ error }));
+});
+
+
+
+module.exports = app;
